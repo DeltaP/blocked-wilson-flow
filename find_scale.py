303048061eb0fb7e12a5eac2733969666303df69
Gregory <gregpetrop@gmail.com>
2012-10-04 16:45:34 -0600

    working on writing python scripts to find scale and do two lattice matching analysis

---
+++ find_scale.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+from string import *
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+t_phys = 0.16;
+w_phys = 0.17;
+
+
---
+++ wflow_2lattice_matching.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+from string import *
+from math import *
+
+c = [0.1, 0.2, 0.3, 0.4]
+L = [12, 16, 24]
+
+t_m = math.pow((c * L),2) / 8
+
