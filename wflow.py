4a2158a4b90727b9c0ec61b7367769a9b6e443f8
Gregory Petropoulos <gregpetrop@gmail.com>
2013-07-10 18:59:12 -0600

    some change

--- wflow.py
+++ wflow.py
@@ -10,15 +10,15 @@ import argparse
 from collections import defaultdict
 
 s = 2
-
+dc = 0
 c = 0.3
 
 parser = argparse.ArgumentParser(description='Wilson Flow Matching.')
 parser.add_argument('volume', metavar='volume', type=int, nargs=1, help='Enter the volume you are intersted in.')
-parser.add_argument('tag', metavar='tag', type=str, nargs=1, help='Enter the base name of the run you are intersted in.')
-parser.add_argument('tsmear', metavar='tsmear', type=float, nargs=1, help='Enter the Wflow smearing time you are interested in.')
 args = parser.parse_args()
 argdict = vars(args)
 volume = argdict['volume'].pop()
-tag = argdict['tag'].pop()
-tsmear = argdict['tsmear'].pop()
+
+t2E = 
+
+g2 = (128*pi**2*t2E) / (3*(3-1)*(1+dc)
