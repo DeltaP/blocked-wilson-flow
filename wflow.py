f8f6578855256caa34a348b6427e2caf3e7b770e
Gregory Petropoulos <gregpetrop@gmail.com>
2013-07-23 02:46:45 -0600

    have a working version of wflow.py.  should produce correct plots

--- wflow.py
+++ wflow.py
@@ -19,7 +19,7 @@ s  = 2
 dc = 0
 
 #fitfunc = lambda c, x: -1 / (c[0] + c[1]*(12.0/x) + c[2]*(12.0/x)**2 + c[3]*(12.0/x)**3 - x/12.0)
-fitfunc = lambda c, x: c[0] + c[1]*x + c[2]*x**2 + c[3]*x**3
+fitfunc = lambda c, x: c[3] + c[2]*x + c[1]*x**2 + c[0]*x**3
 errfunc = lambda c, x, y, err: (fitfunc(c, x) - y) / err
 c_in = [1.0, 1.0, 1.0, 1.0]   # Order-of-magnitude initial guesses
 
@@ -32,13 +32,17 @@ c = argdict['c'].pop()
 vol = defaultdict(list)
 vol = argdict['volumes']
 
+betal    = defaultdict(list)
 g2       = defaultdict(dict)
 g2_err   = defaultdict(dict)
+dg2      = defaultdict(dict)
+coeff    = defaultdict(dict)
+pcolor   = {'88': 'b', '1212': 'g', '1616': 'r', '2424': 'c', '3232': 'm', '4848': 'y'}
 
+fig1=plt.figure()
 for volume in vol:
   L = int(volume[:len(volume)/2])
   t = round(((c*L)**2/8)/(0.02))*0.02
-  betal    = defaultdict(list)
   data     = defaultdict(list)
   tmparry  = []
   tmpbetal = []
@@ -53,6 +57,8 @@ for volume in vol:
     data[beta].append(float(tmparry[4]))
 
   betal[volume]=(set(tmpbetal))
+  print volume
+  print betal[volume]
   for b in betal[volume]:
     t2E = np.mean(data[b])
     t2E_err = scipy.stats.sem(data[b])
@@ -62,46 +68,176 @@ for volume in vol:
   x = []
   y = []
   e = []
-  #for b in sorted(betal[volume]):
-    #x.append(float(b))
-    #y.append(float(g2[(volume,b)]))
-    #e.append(float(g2_err[(volume,b)]))
-  #ax = np.array(x)
-  #ay = np.array(y)
-  #ae = np.array(e)
-  #coeff, success = optimize.leastsq(errfunc, c_in[:], args=(ax, ay, ae))
-  #plt.errorbar(ax, ay, yerr=ae, linestyle='None', marker='.', label=volume)
-  #plt.plot(ax, fitfunc(coeff,ax))
-#plt.legend()
-#plt.savefig("plots/wflow.png", format='png')
+  for b in sorted(betal[volume]):
+    x.append(float(b))
+    y.append(float(g2[(volume,b)]))
+    e.append(float(g2_err[(volume,b)]))
+  ax = np.array(x)
+  ay = np.array(y)
+  ae = np.array(e)
+  splt1 = fig1.add_subplot(111)
+  splt1.errorbar(ax, ay, yerr=ae, linestyle='None', marker='.', color=pcolor[volume], label=volume)
+  if ((volume != '4848')and(volume != '3232')):
+    coeff[volume], success = optimize.leastsq(errfunc, c_in[:], args=(ax, ay, ae))
+    splt1.plot(ax, fitfunc(coeff[volume],ax))
+splt1.legend()
+fig1.savefig("plots/wflow.png", format='png')
+
+g0_3232_s2 = []
+if (('88' in vol) and ('1616' in vol) and ('3232' in vol)):
+  print "Matching 88, 1616, 3232"
+  for b in sorted(betal['3232']):
+    tempg = g2[('1616',b)]
+    g0rnd = round(tempg,3)
+    g0_3232_s2.append(g0rnd)
+    print b + "\t" + str(g0rnd)
+    dg2[('2','1616',g0rnd)] = (g2[('3232',b)]-g2[('1616',b)])/(2*np.log(2))
+    x = []
+    y = []
+    e = []
+    for bb in sorted(betal['88']):
+      x.append(float(bb))
+      yval = float(g2[('88',bb)])-float(tempg)
+      y.append(yval)
+      e.append(float(g2_err[('88',bb)]))
+    nx = np.array(x)
+    ny = np.array(y)
+    ne = np.array(e)
+    scoeff, success = optimize.leastsq(errfunc, c_in[:], args=(nx, ny, ne))
+    roots = np.roots(scoeff)
+    possible = []
+    for i in roots:
+      if ((i < float(sorted(betal['88'])[0])) or (i > float(sorted(betal['88'])[-1]))):
+        next
+      else:
+        possible.append(i) 
+    if len(possible) == 1:
+      beta_tmp = possible.pop()
+      dg2[('2','88',g0rnd)] = (np.polyval(coeff['1616'],beta_tmp)-np.polyval(coeff['88'],beta_tmp))/(2*np.log(2))
+    else:
+      print "Did not find one crossing"
+    x = []
+    y = []
+    e = []
+    for bb in sorted(betal['1212']):
+      x.append(float(bb))
+      yval = float(g2[('1212',bb)])-float(tempg)
+      y.append(yval)
+      e.append(float(g2_err[('1212',b)]))
+    nx = np.array(x)
+    ny = np.array(y)
+    ne = np.array(e)
+    scoeff, success = optimize.leastsq(errfunc, c_in[:], args=(nx, ny, ne))
+    roots = np.roots(scoeff)
+    possible = []
+    for i in roots:
+      if ((i < float(sorted(betal['1212'])[0])) or (i > float(sorted(betal['1212'])[-1]))):
+        next
+      else:
+        possible.append(i) 
+    if len(possible) == 1:
+      beta_tmp = possible.pop()
+      dg2[('2','1212',g0rnd)] = (np.polyval(coeff['2424'],beta_tmp)-np.polyval(coeff['1212'],beta_tmp))/(2*np.log(2))
+    else:
+      print "Did not find one crossing"
+
+g0_4848_s2 = []
+if (('1212' in vol) and ('2424' in vol) and ('4848' in vol)):
+  print "Matching 1212, 2424, 4848"
+  for b in sorted(betal['4848']):
+    tempg = g2[('2424',b)]
+    g0rnd = round(tempg,3)
+    g0_4848_s2.append(g0rnd)
+    print b + "\t" + str(g0rnd)
+    dg2[('2','2424',g0rnd)] = (g2[('4848',b)]-g2[('2424',b)])/(2*np.log(2))
+    x = []
+    y = []
+    e = []
+    for bb in sorted(betal['1212']):
+      x.append(float(bb))
+      yval = float(g2[('1212',bb)])-float(tempg)
+      y.append(yval)
+      e.append(float(g2_err[('1212',bb)]))
+    nx = np.array(x)
+    ny = np.array(y)
+    ne = np.array(e)
+    scoeff, success = optimize.leastsq(errfunc, c_in[:], args=(nx, ny, ne))
+    roots = np.roots(scoeff)
+    possible = []
+    for i in roots:
+      if ((i < float(sorted(betal['1212'])[0])) or (i > float(sorted(betal['1212'])[-1]))):
+        next
+      else:
+        possible.append(i) 
+    if len(possible) == 1:
+      beta_tmp = possible.pop()
+      dg2[('2','1212',g0rnd)] = (np.polyval(coeff['2424'],beta_tmp)-np.polyval(coeff['1212'],beta_tmp))/(2*np.log(2))
+    else:
+      print "Did not find one crossing"
+    x = []
+    y = []
+    e = []
+    for bb in sorted(betal['88']):
+      x.append(float(bb))
+      yval = float(g2[('88',bb)])-float(tempg)
+      y.append(yval)
+      e.append(float(g2_err[('88',bb)]))
+    nx = np.array(x)
+    ny = np.array(y)
+    ne = np.array(e)
+    scoeff, success = optimize.leastsq(errfunc, c_in[:], args=(nx, ny, ne))
+    roots = np.roots(scoeff)
+    possible = []
+    for i in roots:
+      if ((i < float(sorted(betal['88'])[0])) or (i > float(sorted(betal['88'])[-1]))):
+        next
+      else:
+        possible.append(i) 
+    if len(possible) == 1:
+      beta_tmp = possible.pop()
+      dg2[('2','88',g0rnd)] = (np.polyval(coeff['1616'],beta_tmp)-np.polyval(coeff['88'],beta_tmp))/(2*np.log(2))
+    else:
+      print "Did not find one crossing"
 
 
-x = []
-y = []
-x.append(1.0/24**2)
-y.append((g2[('4848','6.0')]-g2[('2424','6.0')])/(2*np.log(2)))
-x.append(1.0/16**2)
-y.append((g2[('3232','6.0')]-g2[('1616','6.0')])/(2*np.log(2)))
-x.append(1.0/12**2)
-y.append((g2[('2424','6.0')]-g2[('1212','6.0')])/(2*np.log(2)))
-x.append(1.0/8**2)
-y.append((g2[('1616','6.0')]-g2[('88','6.0')])/(2*np.log(2)))
+if (('88' in vol) and ('1212' in vol) and ('1616' in vol) and ('2424' in vol) and ('3232' in vol) and ('4848' in vol)):
+  print "Matching 88, 1212, 1616, 2424, 3232, 4848"
 
-ax = np.array(x)
-ay = np.array(y)
-plt.plot(ax, ay)
-plt.savefig('plots/six.png', format='png')
 
-#x = []
-#y = []
-#x.append(1.0/16**2)
-#y.append((g2[('3232','4.0')]-g2[('1616','4.0')])/(2*np.log(2)))
-#x.append(1.0/12**2)
-#y.append((g2[('2424','4.0')]-g2[('1212','4.0')])/(2*np.log(2)))
-#x.append(1.0/8**2)
-#y.append((g2[('1616','4.0')]-g2[('88','4.0')])/(2*np.log(2)))
+print "plot 3232 s=2"
+for g0 in g0_3232_s2:
+  x = []
+  y = []
+  x.append(1.0/16**2)
+  y.append(dg2[('2', '1616',g0)])
+  x.append(1.0/12**2)
+  y.append(dg2[('2', '1212',g0)])
+  x.append(1.0/8**2)
+  y.append(dg2[('2', '88',g0)])
 
-#ax = np.array(x)
-#ay = np.array(y)
-#plt.plot(ax, ay)
-#plt.savefig('plots/four.png', format='png')
+  ax = np.array(x)
+  ay = np.array(y)
+  fig2 = plt.figure()
+  splt2 = fig2.add_subplot(111)
+  splt2.plot(ax, ay)
+  fig2.savefig('plots/3232_'+ str(g0)+'.png', format='png')
+
+print "plot four.png s=2"
+for g0 in g0_4848_s2:
+  x = []
+  y = []
+  x.append(1.0/24**2)
+  y.append(dg2[('2', '2424',g0)])
+  #x.append(1.0/16**2)
+  #y.append(dg2[('2', '1616',g0)])
+  x.append(1.0/12**2)
+  y.append(dg2[('2', '1212',g0)])
+  x.append(1.0/8**2)
+  y.append(dg2[('2', '88',g0)])
+
+  ax = np.array(x)
+  ay = np.array(y)
+  fig3 = plt.figure()
+  splt3 = fig3.add_subplot(111)
+  splt3.plot(ax, ay)
+  fig3.savefig('plots/4848_'+ str(g0)+'.png', format='png')
